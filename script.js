497c0c8b3f77eb9a56d89b5af003089776c6874b
Elise Moura <88293904+lihmooura@users.noreply.github.com>
2021-09-17 10:45:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 var css= document.querySelector("h3");
 var color1= document.querySelector(".color1");
 var color2= document.querySelector(".color2");
 var body = document.getElementById("gradient");
+
 
 function setGradient(){
     body.style.background = 
@@ -11,10 +13,14 @@ function setGradient(){
     + color2.value 
     + ")";
 
+    
     css.textContent = body.style.background + ";";
 }
 
+
+
 color1.addEventListener("input", setGradient);
+
 
 color2.addEventListener("input", setGradient);
 
